89e30050e9ef1de4ed578da76133351db62875d8
Aura-Ch <aura.dev.ch@gmail.com>
2021-03-05 04:20:09 +0100

    Update ExceptionSingleFile.hpp

--- ExceptionSingleFile.hpp
+++ ExceptionSingleFile.hpp
@@ -75,7 +75,7 @@ namespace Lux
 
         #define try Lux::Exception::Try();{
         #define throw(x, y) return Lux::Exception::Throw(x) ? y : y
-        #define nrethrow(x) Lux::Exception::Throw(x)
+        #define nrethrow(x) Lux::Exception::Throw(x); return;
         #define catch(x, ...) }if(Lux::Exception::Catch(x, ##__VA_ARGS__))
         #define any }if(Lux::Exception::CatchAny())
         #define exception(x) enum class x : Lux::u32
